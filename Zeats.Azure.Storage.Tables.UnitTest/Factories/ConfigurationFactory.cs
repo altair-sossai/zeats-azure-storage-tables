ea46b4e674123ce760d9039086cef4277408248b
Altair Sossai <altairsossai@gmail.com>
2021-02-05 22:31:33 -0300

    Atualização de pacotes

---
+++ Zeats.Azure.Storage.Tables.UnitTest/Factories/ConfigurationFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Zeats.Azure.Storage.Tables.UnitTest.Factories
+{
+    public static class ConfigurationFactory
+    {
+        public static IConfiguration New()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            var builder = configurationBuilder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+    }
+}
--- Zeats.Azure.Storage.Tables.UnitTest/Factories/UnitOfWorkAzureStorageFactory.cs
+++ Zeats.Azure.Storage.Tables.UnitTest/Factories/UnitOfWorkAzureStorageFactory.cs
@@ -1,4 +1,5 @@
-﻿using Zeats.Azure.Storage.Tables.UnitOfWork;
+﻿using Microsoft.Extensions.Configuration;
+using Zeats.Azure.Storage.Tables.UnitOfWork;
 
 namespace Zeats.Azure.Storage.Tables.UnitTest.Factories
 {
@@ -6,7 +7,8 @@ namespace Zeats.Azure.Storage.Tables.UnitTest.Factories
     {
         public static UnitOfWorkAzureStorageTables New()
         {
-            const string connectionString = "DefaultEndpointsProtocol=https;AccountName=zeatslauncherstorage;AccountKey=Rv7HvqP9C2u3RYBMw0OhLIrHQJvyz3Ae9Yuz8233geDkdtyYdGDWpM74uu6mqRfF0+h0TX5jX27l0cTn9fok+g==;EndpointSuffix=core.windows.net";
+            var configuration = ConfigurationFactory.New();
+            var connectionString = configuration.GetValue<string>("AzureWebJobsStorage");
 
             return new UnitOfWorkAzureStorageTables(connectionString);
         }
