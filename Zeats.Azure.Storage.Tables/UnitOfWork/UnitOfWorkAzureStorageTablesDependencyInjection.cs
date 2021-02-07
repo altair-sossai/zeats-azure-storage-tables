1728095d3c169ff6d6ac44892caed2227ec09c6d
Altair Sossai <altairsossai@gmail.com>
2021-02-07 12:19:20 -0300

    DependencyInjection

---
+++ Zeats.Azure.Storage.Tables/UnitOfWork/UnitOfWorkAzureStorageTablesDependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Zeats.Azure.Storage.Tables.UnitOfWork
+{
+    public static class UnitOfWorkAzureStorageTablesDependencyInjection
+    {
+        public static IServiceCollection AddUnitOfWorkAzureStorageTables(this IServiceCollection serviceCollection, string connectionString)
+        {
+            serviceCollection
+                .AddScoped(s => new UnitOfWorkAzureStorageTables(connectionString));
+
+            return serviceCollection;
+        }
+    }
+}
